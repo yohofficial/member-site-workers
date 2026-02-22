6573395e14c7689f0a8354f6fb8a2f0f016aeec8
yohofficial <info.yoh.official@gmail.com>
2026-02-23 04:01:35 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,10 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    // 🔐 LINEログイン開始
     if (url.pathname === "/login") {
-      const redirectUri = `${url.origin}/callback`;
-
-      const lineLoginUrl =
-        "https://access.line.me/oauth2/v2.1/authorize" +
-        `?response_type=code` +
-        `&client_id=${env.LINE_CHANNEL_ID}` +
-        `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-        `&scope=profile%20openid`;
-
-      return Response.redirect(lineLoginUrl, 302);
+      return new Response("LOGIN BRANCH HIT");
     }
 
-    // それ以外は静的配信
-    return env.ASSETS.fetch(request);
+    return new Response("ROOT BRANCH");
   },
 };
